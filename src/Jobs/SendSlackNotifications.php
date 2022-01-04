6eabc417a5b11c1432ca3a8679e48c576ea7e5b6
Thiago Lopes <thiago@sphere-sante.com>
2022-01-04 15:25:01 -0300

    bugfix autoloading

--- src/Jobs/SendSlackNotifications.php
+++ src/Jobs/SendSlackNotifications.php
@@ -10,7 +10,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Developes\Slack\Models\SlackNotification;
 
-class SendSlackNotification implements ShouldQueue
+class SendSlackNotifications implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
